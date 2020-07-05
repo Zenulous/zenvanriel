454ca26644478aabc0a4db828053c69b198f7eb7
Zenulous <zenvanriel@gmail.com>
2020-07-05 15:17:19 +0200

    Improved error page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,21 @@
 import * as React from "react";
+import DefaultNavbar from "../components/DefaultNavbar";
+import {CenteredFlexBox} from "../styles/CenteredFlexBox";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faSadTear} from "@fortawesome/free-solid-svg-icons";
+import DefaultFooter from "../components/DefaultFooter";
 
 export default function NotFoundPage() {
-  return <div>Oopsie! I can't find this page.</div>;
+  return (
+    <div>
+      <DefaultNavbar />
+      <div className="container">
+        <CenteredFlexBox>
+          <FontAwesomeIcon icon={faSadTear} size="10x"></FontAwesomeIcon>
+          <p>Oopsie! I can't find this page.</p>
+        </CenteredFlexBox>
+      </div>
+      <DefaultFooter />
+    </div>
+  );
 }
