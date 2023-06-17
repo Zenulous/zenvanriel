7df332f2c4a2d0d0e2f0fdf671d32ac41760caa1
Zenulous <zenvanriel@gmail.com>
2023-06-17 12:53:38 +0200

    Initial Professional Timeline

--- src/components/TimelineItem.tsx
+++ src/components/TimelineItem.tsx
@@ -1,29 +1,47 @@
 import React, { ReactNode } from "react";
 import styled from "styled-components";
 
-const TimelineItemContainer = styled.div<Pick<TimelineItemProps, "position">>`
-  position: relative;
-  z-index: 1;
-  width: 403px;
-  height: 141px;
-  top: ${(props) => props.position.top || 0}px;
-  left: ${(props) => props.position.left || 0}px;
+const TimelineItemContainer = styled.div<
+  Pick<TimelineItemProps, "position" | "width">
+>`
+  position: absolute;
+  z-index: ${(props) => 1000 - props.position.top || 0};
+  width: ${(props) => props.width || 0}px;
+  height: 75px;
+  top: ${(props) => props.position.top || 0}%;
+  left: ${(props) => props.position.left || 0}%;
   background-blend-mode: color-dodge, normal;
-  backdrop-filter: blur(25px);
+  backdrop-filter: blur(300px);
   border-radius: 31px;
 `;
 
-type TimelineItemProps = {
+const DescriptionPopup = styled.div<Pick<TimelineItemProps, "width">>`
+  position: absolute;
+  z-index: 4;
+  width: ${(props) => props.width + 75 || 0}px;
+  min-width: 200px;
+  background-color: rgba(0, 0, 0, 0.8);
+  backdrop-filter: blur(10px);
+  border-radius: 10px;
+  color: white;
+  display: none;
+  word-wrap: break-word;
+  white-space: pre-line;
+`;
+
+export interface TimelineItemProps {
   title: string;
-  description: string;
+  description: string | ReactNode;
   imageComponent: ReactNode;
+  width: number;
   position: {
     top: number;
     left: number;
   };
-};
+}
 
 const TimelineItemContent = styled.div`
+  color: white;
   display: flex;
   height: 100%;
   width: 100%;
@@ -34,7 +52,8 @@ const TimelineItemContent = styled.div`
 `;
 
 const TimelineImage = styled.div`
-  width: 100px;
+  margin-left: 10px;
+  max-height: 50px;
 `;
 
 const TimelineItemText = styled.div`
@@ -42,25 +61,45 @@ const TimelineItemText = styled.div`
   flex-grow: 1;
   margin-left: 5%;
   flex-direction: column;
-  justify-content: space-around;
+  white-space: pre-line;
+  line-height: 20px;
 `;
 
 export default function TimelineItem({
   title,
   description,
   imageComponent,
+  width,
   position,
 }: TimelineItemProps): JSX.Element {
+  console.log(width);
   return (
-    <TimelineItemContainer position={position}>
+    <TimelineItemContainer
+      position={position}
+      width={width}
+      onMouseEnter={(e) => {
+        (
+          e.currentTarget.querySelector(DescriptionPopup) as HTMLElement
+        ).style.display = "flex";
+        (
+          e.currentTarget.querySelector(TimelineItemContent) as HTMLElement
+        ).style.display = "none";
+      }}
+      onMouseLeave={(e) => {
+        (
+          e.currentTarget.querySelector(DescriptionPopup) as HTMLElement
+        ).style.display = "none";
+        (
+          e.currentTarget.querySelector(TimelineItemContent) as HTMLElement
+        ).style.display = "flex";
+      }}
+    >
+      <DescriptionPopup width={width}>
+        <div style={{ padding: "8px" }}>{description}</div>
+      </DescriptionPopup>
       <TimelineItemContent>
         <TimelineImage>{imageComponent}</TimelineImage>
-        <TimelineItemText>
-          <div>
-            <b>{title}</b>
-          </div>
-          <div>{description}</div>
-        </TimelineItemText>
+        <TimelineItemText>{title}</TimelineItemText>
       </TimelineItemContent>
     </TimelineItemContainer>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
-import type { HeadFC, PageProps } from "gatsby";
+import type { PageProps } from "gatsby";
 import { SEO } from "../components/seo";
-import TimelineItem from "../components/TimelineItem";
+import TimelineItem, { TimelineItemProps } from "../components/TimelineItem";
 import { StaticImage } from "gatsby-plugin-image";
 
 const pageStyles = {
@@ -9,77 +9,235 @@ const pageStyles = {
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 };
 
-const jobs = [
+const jobs: TimelineItemProps[] = [
   {
-    id: "1",
-    title: "Software Engineer II",
-    company: "GitHub",
-    duration: "2023 - Present",
-    description: [
-      "Engineering within a team focused on Digital Customer Success.",
-      "Increased self-help for users with any kind of problem on GitHub.",
-      "Automatic Virtual Support.",
-      "Intelligent Search with Natural Language Processing.",
-    ],
+    title: `2023
+GitHub
+Software Engineer II`,
+    imageComponent: (
+      <StaticImage
+        src="../images/githubIcon.svg"
+        alt="GitHub Icon"
+        placeholder="blurred"
+        width={50}
+        height={50}
+      />
+    ),
+    width: 300,
+    position: {
+      top: 25,
+      left: 48,
+    },
+    description: `• Developed Support Copilot: Increased self-help for users with any kind of problem on GitHub
+
+    • Automating Virtual Support with ML & AI
+
+    • Implementing Intelligent Search with Natural Language Processing
+    `,
   },
   {
-    id: "2",
-    title: "Azure Engineer",
-    company: "Beyond Blue",
-    duration: "2022 - 2023",
-    description: [
-      "Developed and delivered cloud-native Kubernetes environments.",
-      "Azure Engineering with a focus on DevOps for De Nederlandsche Bank.",
-    ],
+    title: `2022
+    Beyond Blue
+    Azure DevOps Engineer`,
+    imageComponent: (
+      <StaticImage
+        src="../images/beyondBlue.png"
+        alt="Beyond Blue Icon"
+        placeholder="blurred"
+        width={50}
+        height={50}
+      />
+    ),
+    width: 300,
+    position: {
+      top: 31,
+      left: 13,
+    },
+    description: `•  Developed, delivered and maintained cloud-native Kubernetes environments
+
+    • Azure Engineering with a focus on DevOps for De Nederlandsche Bank
+    `,
   },
   {
-    id: "3",
-    title: "Customer Engineer",
-    company: "Microsoft",
-    duration: "2021 - 2022",
-    description: [
-      "Empowered customers to build cloud-native Azure solutions by collaborating with them.",
-      "Wrote TypeScript, C# code to enable enterprise customers towards cloud-native solutions on Azure.",
-      "Collaborated with 50+ customers across software development, DevOps and cloud architecture.",
-      "Assessed and improved workloads running on Azure.",
-    ],
+    title: `2022
+    SkillScape
+    Co-founder`,
+    imageComponent: (
+      <div style={{ height: "40px", width: "24px", margin: "auto" }}>
+        <StaticImage
+          src="../images/skillscape.png"
+          alt="SkillScape Icon"
+          placeholder="blurred"
+          width={48}
+          height={80}
+        />
+      </div>
+    ),
+    width: 250,
+    position: {
+      top: 40,
+      left: 47,
+    },
+    description: (
+      <div>
+        Connect employees through internal learning opportunities on Microsoft
+        Teams.<br></br>
+        <br></br>
+        <a href="https://skillscape.app" target="_blank">
+          SkillScape
+        </a>{" "}
+        helps employees gain new skills, enrich their network, and get work done
+        faster.
+      </div>
+    ),
   },
   {
-    id: "4",
-    title: "Cognitive Science & AI Research Assistant",
-    company: "Tilburg University",
-    duration: "2020 - 2021",
-    description: [
-      "Developed and delivered a new version of the Python based pseudoword generator Wuggy.",
-    ],
+    title: `2021
+    Microsoft
+    Customer Engineer`,
+    description: `• Wrote TypeScript & Python code to advance enterprise customers towards cloud-native solutions on Azure
+
+      • Collaborated with 50+ customers across software development, DevOps and cloud architecture, including:
+      - Financial Sector (ABN Amro, Rabobank, Handelsbanken)
+      - Government (Ministry of Defense, municipalities)
+      - Healthcare (Leiden University Medical Center)
+      - Education (Eramus University)
+      - Retail (Heineken)
+      - Manufacturing (Philips, ASML)
+      - Transport (ProRail, Maersk)
+      - IT (Atos)
+
+      `,
+    width: 300,
+    position: {
+      top: 48,
+      left: 12,
+    },
+    imageComponent: (
+      <StaticImage
+        src="../images/microsoft.svg"
+        alt="Microsoft Icon"
+        placeholder="blurred"
+        width={50}
+        height={50}
+      />
+    ),
   },
   {
-    id: "5",
-    title: "Cloud Solutions Architect Intern",
-    company: "Microsoft",
-    duration: "2019 - 2020",
-    description: [
-      "Cooperated with international enterprises to develop proofs of concept.",
-      "Lead software engineer on the Brainy project: a Microsoft Teams chatbot.",
-      "Enabled knowledge sharing by presenting internal and external projects.",
-    ],
+    title: `2020
+    Tilburg University
+    AI Research Assistant
+    `,
+    imageComponent: (
+      <StaticImage
+        src="../images/wuggy.png"
+        alt="Wuggy Icon"
+        placeholder="blurred"
+        width={40}
+        height={45}
+      />
+    ),
+    width: 300,
+    position: {
+      top: 57,
+      left: 57,
+    },
+    description: (
+      <div>
+        Developed and delivered a new version of the Python based{" "}
+        <a href="https://github.com/WuggyCode/wuggy" target="_blank">
+          pseudoword generator Wuggy.
+        </a>
+      </div>
+    ),
   },
   {
-    id: "6",
-    title: "Junior Software Tester",
-    company: "CMNTY Corporation",
-    duration: "2018 - 2019",
-    description: [
-      "Provided a fully automated test suite written in JavaScript (Cypress).",
-      "Performed general QA testing for a web-based Community platform.",
-    ],
+    title: `2020
+    Tilburg University - BSc (Cum laude)
+    Cognitive Science & Artificial Intelligence`,
+    imageComponent: (
+      <StaticImage
+        src="../images/tilburgUniversity.png"
+        alt="Tilburg University Icon"
+        placeholder="blurred"
+        width={50}
+        height={50}
+      />
+    ),
+    width: 400,
+    position: {
+      top: 64,
+      left: 11,
+    },
+    description: (
+      <div>
+        Thesis{" "}
+        <a
+          href="https://github.com/Zenulous/luis-test-framework/blob/master/thesis/MLOps%20for%20Chatbots%20-%20Applying%20context%20free%20grammar%20to%20test%20the%20performance%20of%20intent%20recognition%20systems.pdf"
+          target="_blank"
+        >
+          MLOps for Chatbots
+        </a>{" "}
+        - Applying context free grammar to test the performance of intent
+        recognition systems
+      </div>
+    ),
   },
   {
-    id: "7",
-    title: "Employee",
-    company: "McDonald's",
-    duration: "2015",
-    description: ["Employee of the month 2015."],
+    title: `2019
+    Microsoft
+    Cloud Solutions Architect Intern`,
+    description: (
+      <div>
+        • Lead software engineer on the{" "}
+        <a
+          href="https://github.com/microsoft/microsoft-teams-brainy-bot"
+          target="_blank"
+        >
+          Brainy project: a Microsoft Teams chatbot
+        </a>
+        <br></br>
+        <br></br>• Cooperated with international enterprises to develop proofs
+        of concept Azure solutions
+      </div>
+    ),
+    imageComponent: (
+      <StaticImage
+        src="../images/microsoft.svg"
+        alt="Microsoft Icon"
+        placeholder="blurred"
+        width={50}
+        height={50}
+      />
+    ),
+    width: 350,
+    position: {
+      top: 73,
+      left: 56,
+    },
+  },
+  {
+    title: `2018
+    CMNTY
+    Junior Software Tester`,
+    imageComponent: (
+      <StaticImage
+        src="../images/cmnty.png"
+        alt="CMNTY Icon"
+        placeholder="blurred"
+        width={100}
+        height={50}
+      />
+    ),
+    width: 350,
+    description: `• Provided a fully automated test suite written in JavaScript (Cypress)
+
+      • Performed general QA testing for a web-based Community platform
+      `,
+    position: {
+      top: 85,
+      left: 18,
+    },
   },
 ];
 
@@ -87,29 +245,20 @@ const IndexPage: React.FC<PageProps> = () => {
   return (
     <main style={pageStyles}>
       <title>Home Page</title>
-      <div>
-        <TimelineItem
-          title="test"
-          description="this is a long description 123 123 123 123 123123 2 23 1123 312"
-          imageComponent={
-            <StaticImage
-              src="../images/githubIcon.svg"
-              alt="GitHub Icon"
-              placeholder="blurred"
-              width={100}
-              height={100}
-            />
-          }
-          position={{
-            top: 490,
-            left: 150,
-          }}
-        ></TimelineItem>
+      <div style={{ position: "relative", maxWidth: 996 }}>
+        {jobs.map((job) => (
+          <TimelineItem
+            title={job.title}
+            width={job.width}
+            description={job.description}
+            imageComponent={job.imageComponent}
+            position={job.position}
+          ></TimelineItem>
+        ))}
         <StaticImage
           src="../images/river.png"
           alt="River Image"
           placeholder="blurred"
-          layout="fixed"
         />
       </div>
     </main>
