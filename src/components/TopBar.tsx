ccf10e071262d98a5ad676b3f1918b6496663e15
Zenulous <zenvanriel@gmail.com>
2023-06-18 14:33:08 +0200

    Fix topbar for mobile responsiveness

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,7 +1,7 @@
 import { Link } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import styled, { keyframes } from "styled-components";
 
 const TopBarContainer = styled.div`
   display: flex;
@@ -26,15 +26,31 @@ const SiteName = styled.div`
   margin-left: 25px;
 `;
 
-const MenuItems = styled.ul`
+const MenuItems = styled.ul<{ isMenuOpen: boolean }>`
   list-style-type: none;
   display: flex;
   gap: 10px;
   margin-right: 25px;
   padding: 0;
+
+  @media (max-width: 768px) {
+    display: ${({ isMenuOpen }) => (isMenuOpen ? "flex" : "none")};
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 8px;
+    position: absolute;
+    top: 80%;
+    right: 0;
+    background-color: #f1f1f1;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
+    padding: 10px;
+    width: 100%;
+    max-width: 200px; /* Add this line */
+    overflow-x: hidden; /* Add this line */
+  }
 `;
 
-const MenuItem = styled.li`
+const MenuItem = styled.li<{ isMenuOpen: boolean }>`
   font-size: 17px;
   position: relative;
   margin-left: 10px;
@@ -43,18 +59,47 @@ const MenuItem = styled.li`
   &:not(:last-child)::after {
     content: "";
     position: absolute;
-    width: 6px;
+    width: ${({ isMenuOpen }) => (isMenuOpen ? "0" : "6px")};
     height: 6px;
     border-radius: 50%;
     background-color: #0056b3;
     top: 50%;
-
     right: -8px;
     transform: translateY(-50%);
   }
 `;
 
+const rotateAnimation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(90deg);
+  
+  }
+`;
+
+const HamburgerMenu = styled.div<{ isMenuOpen: boolean }>`
+  display: none;
+  margin-right: 25px;
+  fill: #0056b3;
+  cursor: pointer;
+
+  @media (max-width: 768px) {
+    align-self: flex-end;
+    display: inline;
+    animation: ${({ isMenuOpen }) => (isMenuOpen ? rotateAnimation : "none")}
+      0.5s forwards;
+  }
+`;
+
 export function TopBar() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <TopBarContainer>
       <Link to="/">
@@ -69,14 +114,25 @@ export function TopBar() {
         </SiteName>
       </Link>
 
-      <MenuItems>
-        <MenuItem>
+      <HamburgerMenu isMenuOpen={isMenuOpen} onClick={toggleMenu}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="32px"
+          height="32px"
+        >
+          <path d="M0 0h24v24H0z" fill="none" />
+          <path d="M3 18h18v-2H3v2zM3 13h18v-2H3v2zM3 6v2h18V6H3z" />
+        </svg>
+      </HamburgerMenu>
+      <MenuItems isMenuOpen={isMenuOpen}>
+        <MenuItem isMenuOpen={isMenuOpen}>
           <Link to="/">Professional Timeline</Link>
         </MenuItem>
-        <MenuItem>
+        <MenuItem isMenuOpen={isMenuOpen}>
           <Link to="/browser-game">Browser Game</Link>
         </MenuItem>
-        <MenuItem>
+        <MenuItem isMenuOpen={isMenuOpen}>
           <Link to="/certifications">Certifications</Link>
         </MenuItem>
       </MenuItems>
